a86a11e57723117de07bfdeddc9da901addf1e52
Rajith WIjepura <rajithwijepura@users.noreply.github.com>
2025-05-21 10:46:37 +0530

    Deploy application to Netlify

--- src/components/LeadsStats.tsx
+++ src/components/LeadsStats.tsx
@@ -60,7 +60,7 @@ export const LeadsStats: React.FC<LeadsStatsProps> = ({ leadCounts }) => {
         </div>
       </div>
 
-      <div className="bg-gray-800 rounded-xl border border-gray-700 p-6 flex items-center">
+      <div className="bg-black rounded-xl border border-gray-700 p-6 flex items-center">
         <div className="rounded-xl bg-rose-500/20 p-3 mr-4">
           <MessageCircle size={24} className="text-rose-400" />
         </div>
